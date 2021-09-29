cc6a5ae148334c818325fc03001e46d6ddb430a0
Daniel <danbapp@gmail.com>
2021-09-29 11:16:07 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Write your solution in this file!
+const employee = {
+  name: 'Daniel',
+  address: 'danielsAddress',
+};
+
+function updateEmployeeWithKeyAndValue(employee, name, Tom) {
+  const newEmployee = { ...employee };
+}
